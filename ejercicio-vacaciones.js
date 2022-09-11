1a1b44718b4747e23ee5e0871f34bf955f149d51
emperatrizm <emperatriz.668@gmail.com>
2022-09-10 20:20:03 -0500

    ternary-operator

---
+++ ejercicio-vacaciones.js
@@ -0,0 +1,11 @@
+/*Estás planeando unas vacaciones en agosto.
+Se le proporciona un programa que toma el mes como entrada.Complete el código para generar "vacaciones", si el mes dado es Agosto.
+No emita nada de lo contrario.
+Entrada de muestra agosto/ Salida de muestra vacaciones.*/
+
+
+//Operador terniario: es el único operador en JavaScript que tiene tres operandos. Este operador se usa con frecuencia como atajo para 
+//la instrucción if.
+
+        let month = 'August';
+        let holidays = month == "August" ? console.log("it's vacation") : console.log("It's not a vacation");
